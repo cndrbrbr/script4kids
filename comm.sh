2beefdbfc7ad3d1ec073e057d31fd18e0e8bcf65
cndrbrbr <info@screenpaper.de>
2026-02-28 11:49:58 +0100

    c

--- comm.sh
+++ comm.sh
@@ -5,3 +5,6 @@ git commit -m "c $*"
 git push
 
 # git tag -a v1.0.1 -m "Release v1.0.1"
+# git push origin --tags
+# gh release upload v1.0.1 /home/mint/spigot/plugins/jsmn.jar
+# gh release create v1.0.1 build/jsmn.jar   --title "v1.0.1"   --notes "Initial release" 
